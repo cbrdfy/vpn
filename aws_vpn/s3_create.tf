ba06043f756b10208ecd942974649254517f72f3
dfy24 <deify2430@gmail.com>
2024-02-18 15:33:30 -0500

    add makefile

--- aws_vpn/s3_create.tf
+++ aws_vpn/s3_create.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "wg-vpn-bucket" {
   bucket = var.aws_s3_bucket_name
   # Required for destroying not empty bucket
-  # force_destroy = true
+  force_destroy = true
   tags = {
     Name = "VPN bucket"
   }
